9db130c1dac051adf4698e0dfd8ac1f391097cf4
data-doge <eugene@enspiral.com>
2016-08-19 19:03:19 -0400

    cutting video into second long slices

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 var fs = require('fs')
 var youtubedl = require('youtube-dl')
+var exec = require('child_process').exec
+var range = require('lodash.range')
+var moment = require('moment')
 
 var url = process.argv[2].replace('https://', 'http://')
-var clipLength = process.argv[3]
-var filename = process.argv[4]
+var clipLength = 1
+var filename = "finished.mp4"
 
 var video = youtubedl(url, ['--format=18'])
 
+var duration
+
 video.on('info', function (info) {
-  console.log('Download started')
-  console.log('filename: ' + info._filename)
-  console.log('size: ' + info.size)
+  duration = info.duration
 })
 
-video.pipe(fs.createWriteStream(filename))
+video.on('end', function (info) {
+  fs.mkdir('tmp', function () {
+    range(duration).forEach(function (sec) {
+      var startTime = moment().set('second', sec).format('HH:mm:ss')
+      exec('ffmpeg -i tmp.mp4 -ss ' + startTime + ' -t 00:00:01 -async 1 ./tmp/cut-' + sec + '.mp4')
+    })
+  })
+})
+
+video.pipe(fs.createWriteStream("tmp.mp4"))
