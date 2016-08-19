384300cc4d9245248ccae0953201e2d8b0f27c6d
data-doge <eugene@enspiral.com>
2016-08-19 17:59:51 -0400

    can download video from youtube

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('meow')
+var fs = require('fs')
+var youtubedl = require('youtube-dl')
+
+var url = process.argv[2]
+var clipLength = process.argv[3]
+var filename = process.argv[4]
+
+var video = youtubedl(url, ['--format=18'])
+
+video.on('info', function (info) {
+  console.log('Download started')
+  console.log('filename: ' + info._filename)
+  console.log('size: ' + info.size)
+})
+
+video.pipe(fs.createWriteStream(filename))
