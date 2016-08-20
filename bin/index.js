faf367b433ad1bf70c30006e9f57c027d76d67bb
data-doge <eugene@enspiral.com>
2016-08-20 17:43:42 -0400

    add bin

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var fs = require('fs')
 var youtubedl = require('youtube-dl')
 var exec = require('child_process').exec
@@ -11,18 +13,23 @@ var exists = require('fs-exists-sync')
 var url = process.argv[2].replace('https://', 'http://')
 var clipLength = 1
 var filename = process.argv[3] || 'output.mp4'
+
+var tmpDir = path.join(__dirname, 'tmp')
+var originalVideoPath = path.join(tmpDir, 'original.mp4')
 var finishedFilePath = path.join(process.cwd(), filename)
+var clipListPath = path.join(tmpDir, 'clip-list.txt')
 
 var video = youtubedl(url, ['--format=18'])
 
 var duration
 
-if (exists('./tmp')) {
-  remove.removeSync('./tmp')
+
+if (exists(tmpDir)) {
+  remove.removeSync(tmpDir)
 }
 
-fs.mkdirSync('tmp')
-video.pipe(fs.createWriteStream("./tmp/original.mp4"))
+fs.mkdirSync(tmpDir)
+video.pipe(fs.createWriteStream(originalVideoPath))
 
 video.on('info', function (info) {
   duration = info.duration
@@ -32,7 +39,7 @@ video.on('end', function (info) {
   var tasks = range(duration).map(function (sec) {
     return function (cb) {
       var startTime = moment().set({'hour': 0, 'minute': 0, 'second': sec}).format('HH:mm:ss')
-      var cmd = 'ffmpeg -i ./tmp/original.mp4 -ss ' + startTime + ' -t 00:00:01 -async 1 ./tmp/cut-' + sec + '.mp4'
+      var cmd = 'ffmpeg -i ' + originalVideoPath + ' -ss ' + startTime + ' -t 00:00:01 -async 1 ' + path.join(tmpDir, 'cut-' + sec + '.mp4')
       exec(cmd, function (err) {
         if (err) cb(err)
         cb()
@@ -46,7 +53,6 @@ video.on('end', function (info) {
       return 'file ./cut-' + sec + '.mp4'
     })
 
-    var clipListPath = './tmp/clip-list.txt'
     fs.openSync(clipListPath, 'w+')
     filenamesToAppend.forEach(function (filenameToAppend) {
       fs.appendFileSync(clipListPath, filenameToAppend + '\n')
@@ -55,7 +61,7 @@ video.on('end', function (info) {
     var cmd = 'ffmpeg -f concat -i ' + clipListPath + ' -vcodec copy -acodec copy -y ' + finishedFilePath
     exec(cmd, function (err) {
       if (err) throw err
-      remove.removeSync('./tmp')
+      remove.removeSync(tmpDir)
     })
   })
 })
