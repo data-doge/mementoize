07eceac5f7cdbe78b9f87e1861e53999fdceee72
data-doge <eugene@enspiral.com>
2016-08-20 23:45:02 -0400

    add progress bars

--- src/bin/index.js
+++ src/bin/index.js
@@ -9,6 +9,7 @@ import async from 'async'
 import remove from 'remove'
 import path from 'path'
 import exists from 'fs-exists-sync'
+import ProgressBar from 'progress'
 
 const url = process.argv[2].replace('https://', 'http://')
 const filename = process.argv[3] || 'output.mp4'
@@ -30,7 +31,9 @@ const hhmmssToSeconds = (hhmmss) => {
 
 const video = youtubedl(url, ['--format=18'])
 
+let size
 let duration
+let videoDownloadBar
 
 if (exists(tmpDir)) { remove.removeSync(tmpDir) }
 fs.mkdirSync(tmpDir)
@@ -38,22 +41,42 @@ video.pipe(fs.createWriteStream(originalVideoPath))
 
 video.on('info', (info) => {
   duration = hhmmssToSeconds(info.duration)
+  size = info.size
+
+  videoDownloadBar = new ProgressBar('  downloading video [:bar] :percent :etas', {
+    complete: '=',
+    incomplete: ' ',
+    width: 20,
+    total: size
+  })
+})
+
+
+video.on('data', (chunk) => {
+  if (size) {
+    videoDownloadBar.tick(chunk.length)
+  }
 })
 
 
 video.on('end', (info) => {
-  var clipsProcessed = 0
+  let clipPoints = range(0, duration, clipLength)
+  let clipsProcessingBar = new ProgressBar('  processing video [:bar] :percent :etas', {
+    complete: '=',
+    incomplete: ' ',
+    width: 20,
+    total: clipPoints.length
+  })
 
-  let tasks = range(0, duration, clipLength).map((sec, i, arr) => {
+  let tasks = clipPoints.map((sec, i, arr) => {
     return (cb) => {
-      clipsProcessed++
-      console.log((clipsProcessed / arr.length * 100).toFixed(2) + '%')
       let clipStartTime = formattedSeconds(sec)
       let clipDuration = i === arr.length - 1 ? formattedSeconds(duration - sec) : formattedSeconds(clipLength)
 
       let cmd = `ffmpeg -i ${originalVideoPath} -ss ${clipStartTime} -t ${clipDuration} -async 1 ${path.join(tmpDir, 'cut-' + sec + '.mp4')}`
       exec(cmd, (err) => {
         if (err) { cb(err) }
+        clipsProcessingBar.tick(1)
         cb()
       })
     }
@@ -68,10 +91,13 @@ video.on('end', (info) => {
       fs.appendFileSync(clipListPath, filenameToAppend + '\n')
     })
 
+    console.log('stitching ..')
     let cmd = `ffmpeg -f concat -i ${clipListPath} -vcodec copy -acodec copy -y ${finishedFilePath}`
     exec(cmd, (err) => {
       if (err) throw err
+      console.log('cleanin up ..')
       remove.removeSync(tmpDir)
+      console.log('done !!')
     })
   })
 })
